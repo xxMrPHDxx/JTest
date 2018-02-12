10f968e8bf170210249c1003ba9757fdb50d59d4
xxMrPHDxx <pian.minecraft1@gmail.com>
2018-02-13 00:39:30 +0800

    fixed tests

--- matrix.test.js
+++ matrix.test.js
@@ -1,16 +1,26 @@
 const Matrix = require('./matrix.js');
 
 // Contruct a new Matrix object
-test("Creating Matrix object",(value) => {
+test("Creating Matrix object",(done) => {
 	let matrix = new Matrix(2,2);
-	expect(matrix).toEqual(
-		{
-			rows: 2,
-			cols: 2,
-			data: [
-				[0,0],
-				[0,0]
-			]
-		}
-	);
+	expect(matrix).toEqual({
+		rows: 2,
+		cols: 2,
+		data: [
+			[0,0],
+			[0,0]
+		]
+	});
+	done();
+});
+
+// Static from array
+test("Matrix.fromArray",(done) => {
+	let matrix = Matrix.fromArray([1,2,3,4]);
+	expect(matrix).toEqual({
+		rows: 4,
+		cols: 1,
+		data: [[1],[2],[3],[4]]
+	})
+	done();
 });
